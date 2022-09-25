819ef468245ba6a92c356c5f35135b6b99b416ad
goodcai32 <114305743+goodcai32@users.noreply.github.com>
2022-09-25 20:14:06 +0800

    Update and rename config/index.js to 配置/index.js

---
+++ 配置/index.js
@@ -0,0 +1,22 @@
+{
+# 公众号配置
+# 公众号appId
+"app_id": "wx8b9151422a04ea1b",
+# 公众号appSecret
+"app_secret": "e548f2f5b6b13f93c070f97436c135dc",
+# 模板消息id
+"template_id": "wZ5Pev8m1m8R7sjZkrT34W8zDQnGXpA8Ta6YfBXv6dE",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+"user": ["oCRQ26imEZCu5YDh2Q-fbli3_mhQ","oCRQ26jYJ8HPEqnoAcEo-iVTBQs8"],
+
+# 信息配置
+# 所在省份
+"province": "广东",
+# 所在城市
+"city": "广州花都",
+# 生日1
+"birthday": "2023-06-08",
+
+# 在一起的日子，格式同上
+"love_date": "2021-03-14"
+}
